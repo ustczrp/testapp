c82609f4e5627240bc2c0c390351b37862885f4c
Nuno Serro <ngserro@gmail.com>
2015-09-14 16:24:43 +0100

    Database connection trial

--- index.php
+++ index.php
@@ -3,7 +3,20 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>This is just an Azure Web app test!</p>'; ?> 
+ <?php echo '<p>This is just an Azure Web app test!</p>'; 
+$serverName = "gr2nwytk2y"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"ngdb", "UID"=>"webappdb", "PWD"=>"Mydbpassword123");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+
+
+?>
  <?php phpinfo(); ?>
  </body>
 </html>
